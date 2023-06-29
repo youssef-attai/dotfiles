fc1d2962f97cbfc38930dad88e46477946c42c1e
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-29 21:35:55 +0300

    Fix polybar ram module to show usage like htop

--- .config/polybar/scripts/free-mem.sh
+++ .config/polybar/scripts/free-mem.sh
@@ -1,3 +1,21 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-free -h | grep Mem: | awk '{print $3"B"}' | awk '{print substr($0, 0, length($0)-3) " " substr($0, length($0)-2, length($0)) }'
+MemTotal=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+MemFree=$(grep MemFree /proc/meminfo | awk '{print $2}')
+Buffers=$(grep Buffers /proc/meminfo | awk '{print $2}')
+Cached=$(grep "^Cached" /proc/meminfo | awk '{print $2}')
+Shmem=$(grep "^Shmem:" /proc/meminfo | awk '{print $2}')
+SReclaimable=$(grep "^SReclaimable" /proc/meminfo | awk '{print $2}')
+
+# Calculate used memory, similar to htop
+# Convert from kB to MB
+UsedMem=$(((MemTotal + Shmem - MemFree - Buffers - Cached - SReclaimable) / 1024))
+
+# If used memory is less than 1 GB, display it in MB
+if [ $UsedMem -lt 1024 ]; then
+    echo "$UsedMem MB"
+else
+    # Convert from MB to GB with two decimal places
+    UsedMemGB=$(echo "scale=2; $UsedMem / 1024" | bc)
+    echo "$UsedMemGB GB"
+fi
