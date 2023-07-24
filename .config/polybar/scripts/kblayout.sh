03aef9ece5c91bf6241d4a1c226277fabc0cc45c
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-24 16:02:29 +0300

    add keyboard layout to polybar

---
+++ .config/polybar/scripts/kblayout.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+on_off=$(xset -q | grep Group | awk '{print $4}')
+
+if [ "$on_off" = "on" ]
+then
+  echo "AR"
+elif [ "$on_off" = "off" ]
+then
+  echo "US"
+else
+  notify-send "Something went wrong with a polybar script" "Your custom kblayout.sh script is not working as expected" -u critical
+fi
