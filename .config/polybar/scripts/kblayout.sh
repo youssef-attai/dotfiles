9d812ac72b7b16f4b0ec593a33a128af51860979
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-30 15:29:30 +0300

    fix error message

--- .config/polybar/scripts/kblayout.sh
+++ .config/polybar/scripts/kblayout.sh
@@ -9,5 +9,5 @@ elif [ "$on_off" = "off" ]
 then
   echo "US"
 else
-  notify-send "Something went wrong with a polybar script" "Your custom kblayout.sh script is not working as expected" -u critical
+  notify-send "Something went wrong with polybar" "Your custom kblayout.sh script is not working as expected" -u critical
 fi
